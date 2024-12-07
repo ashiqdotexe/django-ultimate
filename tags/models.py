8ab122c5e23478813967875dbf4f066e7ad82daf
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-12-07 13:53:16 +0600

    Generic

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+"""
+Generic relationship
+"""
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
 
-# Create your models here.
+class TagItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
