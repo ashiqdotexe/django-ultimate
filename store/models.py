10789dda989b005c3fd1126f49b68ebf39a2af5f
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-12-09 22:50:11 +0600

    SQLite added

--- store/models.py
+++ store/models.py
@@ -24,7 +24,7 @@ class Promtion(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     product = models.ManyToManyField(Promtion)
@@ -49,6 +49,7 @@ class Customer(models.Model):
 class Adress(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
     
