70016f898d754ea0b38a4a5a99e88b3520773169
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-12-12 23:16:39 +0600

    Query set using string

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
-
+from store.models import Customer
 
 
 
 def say_hello(request):
     query_set = Product.objects.filter(unit_price__range=(1,1000))
-    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
+    query_set2 = Customer.objects.filter(membership__icontains = 'B')
+    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set), 'customers' : query_set2})
