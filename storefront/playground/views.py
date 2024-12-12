89ed0d07ed643ccde2f69e2618e5336787ca0614
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-12-12 22:57:40 +0600

    Query Set

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    query_set = Product.objects.filter(unit_price__range=(1,1000))
+    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
