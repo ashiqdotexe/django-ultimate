c233ef79ed99d9520dd28c394ec0da4b7158bd48
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-12-14 10:34:56 +0600

    Query set with Q method

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
 from store.models import Customer
-
+from django.db.models import Q
 
 
 def say_hello(request):
     query_set = Product.objects.filter(unit_price__range=(1,1000))
-    query_set2 = Customer.objects.filter(membership__icontains = 'B')
+    query_set2 = Customer.objects.filter(Q(membership__icontains = 'B') & Q(first_name__startswith="F"))
     return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set), 'customers' : query_set2})
