7fe21bcf7ebd117857ae1851e0b0d453be0cac5c
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2024-12-22 23:52:09 +0600

    Select_related and prefetch_related added

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 from store.models import Customer
 from django.db.models import Q, F
 
 
 def say_hello(request):
     # query_set = Product.objects.filter(unit_price__range=(1,1000))
-    query_set = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
-    query_set2 = Customer.objects.filter(Q(membership__icontains = 'B') & Q(first_name__startswith="F"))
-    return render(request, 'hello.html', {'name': 'Mosh', 'products':list (query_set), 'customers' : query_set2})
+    query_set = (
+        Order.objects.select_related("customer")
+        .prefetch_related("orderitem_set")
+        .order_by("-placed_at")[:5]
+    )
+    query_set2 = Customer.objects.filter(
+        Q(membership__icontains="B") & Q(first_name__startswith="F")
+    )
+    return render(
+        request,
+        "hello.html",
+        {"name": "Mosh", "orders": list(query_set), "customers": query_set2},
+    )
