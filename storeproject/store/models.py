5488522e9cdcc58ed3dca7e300cc45d37b473139
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-13 22:10:10 +0600

    migration done

--- storeproject/store/models.py
+++ storeproject/store/models.py
@@ -13,7 +13,7 @@ class Promotion(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digit=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
