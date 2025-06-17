e271f540a13d6562b7096a8d33f5b95e1aabb495
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-17 13:46:48 +0600

    Limiting and values query set

--- store/playground/views.py
+++ store/playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product
+from store.models import Product, OrderItem
 def say_hello(request):
-    query_set = Product.objects.filter(title__istartswith="Coffee")
+    # query_set = Product.objects.filter(title__istartswith="Coffee") -- starts with coffee
+    query_set = Product.objects.filter(OrderItem.objects.values("product_id").distinct()).order_by("title")
     return render(request, 'hello.html', {'name': 'Mosh', "products":list(query_set)})
