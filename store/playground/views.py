2d3e391de718b6dc18f68ca617892e5457228171
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-17 18:47:16 +0600

    Annotated added

--- store/playground/views.py
+++ store/playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import F
 from store.models import Product, OrderItem, Order
 def say_hello(request):
     # query_set = Product.objects.filter(title__istartswith="Coffee") -- starts with coffee
     # query_set = Product.objects.filter(id__in=OrderItem.objects.values("product_id").distinct()).order_by("title")[:5]
-    query_set = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
-    return render(request, 'hello.html', {'name': 'Mosh', "orders":list(query_set)})
+    # query_set = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
+    query_set = Product.objects.annotate(new_id = F("id")+1)
+    return render(request, 'hello.html', {'name': 'Mosh', "orders":query_set})
