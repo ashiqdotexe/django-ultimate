ad351dc7fdd5dbe1229e15c602e641fdc0dc9ef7
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-17 15:02:50 +0600

    Select Related and Prefetch added

--- store/playground/views.py
+++ store/playground/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 def say_hello(request):
     # query_set = Product.objects.filter(title__istartswith="Coffee") -- starts with coffee
-    query_set = Product.objects.filter(OrderItem.objects.values("product_id").distinct()).order_by("title")
-    return render(request, 'hello.html', {'name': 'Mosh', "products":list(query_set)})
+    # query_set = Product.objects.filter(id__in=OrderItem.objects.values("product_id").distinct()).order_by("title")[:5]
+    query_set = Order.objects.select_related("customer").prefetch_related("orderitem_set__product").order_by("-placed_at")[:5]
+    return render(request, 'hello.html', {'name': 'Mosh', "orders":list(query_set)})
