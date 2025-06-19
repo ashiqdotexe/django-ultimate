6d0687faf65cbc97f5ae62234ce6d2de50fc01a1
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-19 22:08:54 +0600

    GET Product with ID endpoint

--- store2/store/urls.py
+++ store2/store/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list,product_id
 urlpatterns=[
     path("product/", product_list),
+    path("product/<int:id>", product_id),
 ]
--- store2/store/views.py
+++ store2/store/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from .serializers import ProductSerializer
 from .models import Product
 from rest_framework.response import Response
 
-
 @api_view()
 def product_list(request):
     queryset = Product.objects.all()
     serializer = ProductSerializer(queryset, many = True)
+    return Response(serializer.data)
+@api_view()
+def product_id(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
     return Response(serializer.data)
