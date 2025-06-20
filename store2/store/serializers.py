9f5ad4439eb2ac939b459a10f18ac4e3b4b5aae4
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-20 14:34:05 +0600

    Serializer Advance-1

--- store2/store/serializers.py
+++ store2/store/serializers.py
@@ -1,7 +1,13 @@
+from decimal import Decimal
 from rest_framework import serializers
-
+from .models import Product
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price_on_tax = serializers.SerializerMethodField(
+        method_name = "tax_on_price"
+    )
+    def tax_on_price(self, product: Product):
+        return product.unit_price * Decimal(1.1)
