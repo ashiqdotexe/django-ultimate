1a9809eb78e6aba116604bc28cd20fef9317d139
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-20 20:24:10 +0600

    Model Serializer added

--- store2/store/serializers.py
+++ store2/store/serializers.py
@@ -1,13 +1,36 @@
 from decimal import Decimal
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Collection
 
-class ProductSerializer(serializers.Serializer):
+class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+
+class ProductSerializer(serializers.ModelSerializer):
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     price_on_tax = serializers.SerializerMethodField(
         method_name = "tax_on_price"
     )
+    # #Calling relationalField 3 ways->
+    # #1.
+    # collection = serializers.StringRelatedField()
+    # #2. 
+    # collection = CollectionSerializer()
+    
+    # #3.
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset = Product.objects.all(),
+    #     view_name = "collection-detail"
+    # )
+
+    #Another way to show this-->(ModelSerializer)
+    
+    class Meta:
+        model = Product
+        fields = ["id", "title", "unit_price", "price_on_tax", "collection"]
+
+
     def tax_on_price(self, product: Product):
         return product.unit_price * Decimal(1.1)
--- store2/store/urls.py
+++ store2/store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import product_list,product_id
+from .views import product_list,product_id, collection_detail
 urlpatterns=[
     path("product/", product_list),
     path("product/<int:id>", product_id),
+    path("collection/<int:pk>",collection_detail, name="collection-detail")
 ]
