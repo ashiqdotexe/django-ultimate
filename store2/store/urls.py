6354e58e5b3e4e77e5bde456c4fd7542fdec3308
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-06-29 12:06:57 +0600

    Generic View Implemented

--- store2/store/urls.py
+++ store2/store/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ProductList, product_id, CollectionList, CollectionDetail
+from .views import ProductList, ProductDetail, CollectionList, CollectionDetail
 
 urlpatterns = [
     path("product/", ProductList.as_view()),
-    path("product/<int:id>", product_id),
+    path("product/<int:pk>", ProductDetail.as_view()),
     path("collection/", CollectionList.as_view()),
     path("collection/<int:pk>", CollectionDetail.as_view(), name="collection-detail"),
 ]
--- store2/store/views.py
+++ store2/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.views import APIView
 from .serializers import ProductSerializer, CollectionSerializer
 from .models import Product, Collection
@@ -24,31 +24,31 @@ API View-->
 #         serializer.save()
 #         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class CollectionDetail(APIView):
-    def get(self, request, pk):
-        collection = get_object_or_404(
-        Collection.objects.annotate(product_count=Count("products")), pk=pk)
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-    def put(self, request,pk):
-        collection = get_object_or_404(
-        Collection.objects.annotate(product_count=Count("products")), pk=pk)
-        serializer = CollectionSerializer(collection, request.data)
-        serializer.is_valid()
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
-    def delete(self, request,pk):
-        collection = get_object_or_404(
-        Collection.objects.annotate(product_count=Count("products")), pk=pk)
-        if collection.products.count() > 0:
-            return Response(
-                {"error": "cant delete item because order item exist"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        collection.delete()
-        return Response(
-            {"message": f"item with {pk} deleted"}, status=status.HTTP_204_NO_CONTENT
-        )
+# class CollectionDetail(APIView):
+#     def get(self, request, pk):
+#         collection = get_object_or_404(
+#         Collection.objects.annotate(product_count=Count("products")), pk=pk)
+#         serializer = CollectionSerializer(collection)
+#         return Response(serializer.data)
+#     def put(self, request,pk):
+#         collection = get_object_or_404(
+#         Collection.objects.annotate(product_count=Count("products")), pk=pk)
+#         serializer = CollectionSerializer(collection, request.data)
+#         serializer.is_valid()
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request,pk):
+#         collection = get_object_or_404(
+#         Collection.objects.annotate(product_count=Count("products")), pk=pk)
+#         if collection.products.count() > 0:
+#             return Response(
+#                 {"error": "cant delete item because order item exist"},
+#                 status=status.HTTP_400_BAD_REQUEST,
+#             )
+#         collection.delete()
+#         return Response(
+#             {"message": f"item with {pk} deleted"}, status=status.HTTP_204_NO_CONTENT
+#         )
 """
 GENERIC View->
 """
@@ -60,7 +60,21 @@ class CollectionList(ListCreateAPIView):
     def get_parser_context(self, http_request):
         return {"request": self.request}
 
-
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+    def delete(self, request, pk):
+        collection = get_object_or_404(
+        Collection.objects.annotate(product_count=Count("products")), pk=pk)
+        if collection.products.count() > 0:
+            return Response(
+                {"error": "cant delete item because order item exist"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        collection.delete()
+        return Response(
+            {"message": f"item with {pk} deleted"}, status=status.HTTP_204_NO_CONTENT
+        )
         
 """
 API View-->
@@ -81,29 +95,31 @@ API View-->
 #         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def product_id(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == "GET":
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = ProductSerializer(product, request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
-    elif request.method == "DELETE":
-        if product.orderitem_set.count() > 0:
-            return Response(
-                {"error": "cant delete item because order item exist"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        product.delete()
-        return Response(
-            {"message": f"item with {id} deleted"}, status=status.HTTP_204_NO_CONTENT
-        )
+# @api_view(["GET", "PUT", "DELETE"])
+# def product_id(request, id):
+#     product = get_object_or_404(Product, pk=id)
+#     if request.method == "GET":
+#         serializer = ProductSerializer(product)
+#         return Response(serializer.data)
+#     elif request.method == "PUT":
+#         serializer = ProductSerializer(product, request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == "DELETE":
+#         if product.orderitem_set.count() > 0:
+#             return Response(
+#                 {"error": "cant delete item because order item exist"},
+#                 status=status.HTTP_400_BAD_REQUEST,
+#             )
+#         product.delete()
+#         return Response(
+#             {"message": f"item with {id} deleted"}, status=status.HTTP_204_NO_CONTENT
+#         )
 
-
+"""
+Generic View-->
+"""
 class ProductList(ListCreateAPIView):
     def get_queryset(self):
         return Product.objects.select_related("collection").all()
@@ -111,3 +127,15 @@ class ProductList(ListCreateAPIView):
         return ProductSerializer
     def get_serializer_context(self):
         return {"request": self.request}
+
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    def delete(self, request, pk):
+         product = get_object_or_404(Product, pk=pk)
+         if product.orderitem_set.count()>0:
+             return Response({"message":"can't delete"}, status=status.HTTP_400_BAD_REQUEST)
+         product.delete()
+         return Response(
+            {"message": f"item with {pk} deleted"}, status=status.HTTP_204_NO_CONTENT
+        )
