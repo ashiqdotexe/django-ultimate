d16e1fc3f59d98ac560c251153f976b02d733046
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-07-02 12:50:34 +0600

    Generic Field Added

--- store2/store/views.py
+++ store2/store/views.py
@@ -8,6 +8,8 @@ from .models import Product, Collection, OrderItem, Review
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from .filter import ProductFilter
 """
 API View-->
 """
@@ -150,12 +152,13 @@ GENERIC View->
 Viewset->
 """
 class ProductViewSet(ModelViewSet):
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get("collection_id")
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+    queryset = Product.objects.all()
+    # collection_id = self.request.query_params.get("collection_id")
+    # if collection_id is not None:
+    #     queryset = queryset.filter(collection_id=collection_id)
+    # return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
     serializer_class = ProductSerializer
     def get_serializer_context(self):
         return {"request": self.request}
