778390708332f5f9f7a66d4c3864f652cae1fd8f
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-07-10 23:58:36 +0600

    Profile User-1

--- store2/store/models.py
+++ store2/store/models.py
@@ -2,6 +2,8 @@ from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
 from django.conf import settings
+from django.contrib import admin
+
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -61,8 +63,10 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
+    @admin.display(ordering="user__first_name")
     def first_name(self):
         return self.user.first_name
+    @admin.display(ordering="user__last_name")
     def last_name(self):
         return self.user.last_name
     class Meta:
