8dc4a83896dca91120e344658f0226ab829c7a70
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-07-16 23:20:50 +0600

    Update Order implemented

--- store2/store/serializers.py
+++ store2/store/serializers.py
@@ -138,6 +138,11 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ["id","customer","payment_status","placed_at", "item"]
 
+class UpdateOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ["payment_status"]
+
 class CreateOrderSerializer(serializers.Serializer):
     with transaction.atomic():
     
