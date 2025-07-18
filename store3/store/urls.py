91d6084255d802c5d006d2ab08091d80554bb9ed
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-07-18 23:24:09 +0600

    Upload file setting

--- store3/store/urls.py
+++ store3/store/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.urls.conf import include
 from rest_framework_nested import routers
 from . import views
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
