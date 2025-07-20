f39c6cd8a35b1a225dfb66cf66d63f0fb1929ab2
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-07-20 21:29:20 +0600

    Custom Validator added

--- store3/store/models.py
+++ store3/store/models.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
+from .validators import validate_file_size
 from uuid import uuid4
 
 
@@ -44,7 +45,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to="store/images")
+    image = models.ImageField(upload_to="store/images", validators=[validate_file_size])
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
---
+++ store3/store/validators.py
@@ -0,0 +1,5 @@
+from django.core.exceptions import ValidationError
+def validate_file_size(file):
+    max_size = 100
+    if file.size > max_size*1024:
+        return ValidationError(f'File size can not be more that {max_size} kb')
