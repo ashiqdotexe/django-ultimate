526a1447eeaaf9c4f78aabe065c6f4a0b6f15149
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-07-23 16:29:55 +0600

    Templated email

--- store3/playground/views.py
+++ store3/playground/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.core.mail import send_mail,mail_admins,BadHeaderError
-
+from templated_mail.mail import BaseEmailMessage
 def say_hello(request):
     try:
-        send_mail("Hello From Django", "Hello this is from django", "admin@admin.com", ["ashiqurrahman.sohan.muslim@gmail.com"])
+        message = BaseEmailMessage(
+            template_name= "emails/hello.html",
+            context= {"name":"SOHAN"}
+        )
+        message.send(["ashiq@gmail.com"])
     except BadHeaderError:
         pass 
     return render(request, 'hello.html', {'name': 'Mosh'})
