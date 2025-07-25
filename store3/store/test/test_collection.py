07f52bb962d26894b0ebfa9bd8569ff394a5299a
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-07-25 11:36:05 +0600

    Pytest

---
+++ store3/store/test/test_collection.py
@@ -0,0 +1,9 @@
+from rest_framework.test import APIClient
+from rest_framework import status
+import pytest
+@pytest.mark.django_db
+class TestCollection:
+    def test_if_user_is_anonymous_return_404(self):
+        client = APIClient()
+        response=client.post("/store/collections/", {"title":"A"})
+        assert response.status_code == status.HTTP_201_CREATED
