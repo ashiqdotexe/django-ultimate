5760af8bc70a91fd636a9868021f5126df85d895
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-07-25 23:56:23 +0600

    Testing final phase-1

--- store3/store/test/test_collection.py
+++ store3/store/test/test_collection.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework import status
 import pytest
@@ -13,3 +14,18 @@ class TestCollection:
         client.force_authenticate(user={})
         response=client.post("/store/collections/", {"title":"A"})
         assert response.status_code == status.HTTP_403_FORBIDDEN  
+    
+    def test_if_data_is_not_valid_return_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response=client.post("/store/collections/", {"title":""})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data["title"] is not None
+
+    def test_if_data_is_valid_return_200(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response=client.post("/store/collections/", {"title":"A"})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["id"] > 0
+    
