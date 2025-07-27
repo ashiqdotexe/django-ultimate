1fdaec53a3856fda017d5fea80c102aa58a0c47a
HM Ashiqur Rahman <ashiqurrahman.sohan.muslim@gmail.com>
2025-07-27 23:03:31 +0600

    Class based caching

--- store3/playground/views.py
+++ store3/playground/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.core.mail import send_mail,mail_admins,BadHeaderError
 from .tasks import notify_user
 from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 import requests
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 
-@cache_page(1*60)
-def say_hello(request):
-    requests.get("http://httpbin.org/delay/2")
-    return render(request, 'hello.html', {'name': 'Mosh'})
+class SayHello(APIView):
+    permission_classes = [AllowAny]
+    @method_decorator(cache_page(5*60))
+    def get(self, request):
+        requests.get("http://httpbin.org/delay/2")
+        return render(request, 'hello.html', {'name': 'Mosh'})
+
+
+    
